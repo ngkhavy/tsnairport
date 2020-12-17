77fec3d8fbe3cc0da276a9f339a5bf81775b2ef0
dinhquynhnhu <nhudtq18411@st.uel.edu.vn>
2020-12-18 00:00:44 +0700

    update: bam search -> chuyen sang booking2, bam back quay lại booking

--- app/src/main/java/com/example/home/Booking.java
+++ app/src/main/java/com/example/home/Booking.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 
 import android.text.InputType;
 import android.view.LayoutInflater;
@@ -22,25 +24,41 @@ import java.util.Calendar;
 public class Booking extends Fragment {
     EditText date_input1;
     EditText date_input2;
+    Button btntimkiem;
     public Booking (){
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_booking, container, false);
-        date_input1=view.findViewById(R.id.date_input1);
-        date_input2=view.findViewById(R.id.date_input2);
-        date_input1.setInputType(InputType.TYPE_NULL);
-        date_input1.setOnClickListener(new View.OnClickListener() {
+        View view = inflater.inflate(R.layout.fragment_booking, container, false);
+       date_input1=view.findViewById(R.id.date_input1);
+       date_input2=view.findViewById(R.id.date_input2);
+       date_input1.setInputType(InputType.TYPE_NULL);
+       date_input1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDateDialog(date_input1);
             }
         });
 
+
+        // nút search ra màn hình fragment booking 2
+        btntimkiem=view.findViewById(R.id.btntimkiem);
+        btntimkiem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Booking2 booking2=new Booking2();
+                FragmentTransaction transaction=getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment,booking2);
+                transaction.commit();
+            }
+        });
+
         return view;
     }
+
     private void showDateDialog(EditText date_input1) {
         Calendar calendar = Calendar.getInstance();
         DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
--- app/src/main/java/com/example/home/Booking2.java
+++ app/src/main/java/com/example/home/Booking2.java
@@ -3,62 +3,45 @@ package com.example.home;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Booking2#newInstance} factory method to
- * create an instance of this fragment.
+
  */
 public class Booking2 extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    Layout vietjet;
+    ImageView backtoBooking;
     public Booking2() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Booking2.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Booking2 newInstance(String param1, String param2) {
-        Booking2 fragment = new Booking2();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_booking2, container, false);
+        View view = inflater.inflate(R.layout.fragment_booking2, container, false);
+        backtoBooking=view.findViewById(R.id.backtoBooking);
+        backtoBooking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Booking booking=new Booking();
+                FragmentTransaction transaction=getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment,booking);
+                transaction.commit();
+            }
+        });
+        return view;
+
     }
+
 }
