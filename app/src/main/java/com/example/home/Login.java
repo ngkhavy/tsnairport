66f0dcd48dc1a32605596c208deec3500d10e140
Nguyen Hai Ly <nguyenhailyly00@gmail.com>
2020-12-18 11:26:52 +0700

    Thêm xử lý bấm Login Button ==> Main Activity

--- app/src/main/java/com/example/home/Login.java
+++ app/src/main/java/com/example/home/Login.java
@@ -2,15 +2,32 @@ package com.example.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams);
         setContentView(R.layout.activity_login);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
     }
+    public void openMainActivity(){
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
