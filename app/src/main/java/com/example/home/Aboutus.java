5dfb47e23340106613f113ce3edce88380ccdbb5
Vy Nguyen <vynk18411@st.uel.edu.vn>
2020-12-24 12:40:32 +0700

    update logo.

--- app/src/main/java/com/example/home/Aboutus.java
+++ app/src/main/java/com/example/home/Aboutus.java
@@ -11,7 +11,7 @@ import android.view.ViewGroup;
 
 public class Aboutus extends Fragment {
 
-
+//aboutus
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
